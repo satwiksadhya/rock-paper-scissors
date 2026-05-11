c04f7da2b46b2ed3e6a0a0dfdc81d3511b9cd540
satwiksadhya <141171299+satwiksadhya@users.noreply.github.com>
2026-05-11 10:30:39 +0530

    Update the scoring mechanism Add playGame() function to play 5 rounds Add game instruction in index.html

--- app.js
+++ app.js
@@ -42,28 +42,35 @@ function playRound(humanChoice, computerChoice){
     if((humanChoice === "Rock" && computerChoice === "Scissors")||(humanChoice === "Paper" && computerChoice === "Rock") || (humanChoice === "Scissors" && computerChoice === "Paper")){
         console.log("You Won!");
         console.log("Your Score: " + ++humanScore);
-        if(computerScore > 0){
-            console.log("Computer Score: " + --computerScore);
-        }
-        else{
-            console.log("Computer Score: " + computerScore);
-        }
+        console.log("Computer Score: " + computerScore);
+        console.log("-----------------------------------");
+        return;
     }
     else if(humanChoice === computerChoice){
         console.log("Tie!");
         console.log("Your Score: " + humanScore);
         console.log("Computer Score: " + computerScore);
+        console.log("-----------------------------------");
+        return;
     }
     else{
         console.log("You Lose!");
         console.log("Computer Score: " + ++computerScore);
-        if(humanScore > 0){
-            console.log("Your Score: " + --humanScore);
-        }
-        else{
-            console.log("Your Score: " + humanScore);
-        }
+        console.log("Your Score: " + humanScore);
+        console.log("-----------------------------------");
+        return;
     }
 }
 
-playRound(getHumanChoice(),getComputerChoice());
+function playGame(){
+    for(let i=0; i<5; i++){
+        console.log("Round " + (i+1))
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+    console.log("GAME OVER");
+    humanScore>computerScore?console.log("YOU ARE THE WINNER"):humanScore===computerScore?console.log("MATCH TIE!"):console.log("COMPUTER IS THE WINNER");
+}
+
+playGame()
+
+
